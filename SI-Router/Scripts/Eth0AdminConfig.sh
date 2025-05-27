a549e3e14d0dff0e2e57cec9720b864674501506
chipmanfu <50666533+chipmanfu@users.noreply.github.com>
2025-05-27 15:20:15 -0500

    Update Eth0AdminConfig.sh

--- SI-Router/Scripts/Eth0AdminConfig.sh
+++ SI-Router/Scripts/Eth0AdminConfig.sh
@@ -3,7 +3,7 @@
 source /opt/vyatta/etc/functions/script-template
 
 configure
-set interfaces ethernet eth0 address 172.30.7.254/24
+set interfaces ethernet eth0 address 172.30.7.254/21
 commit
 save
 exit
